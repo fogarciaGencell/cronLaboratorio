661be1ee54256718f064922aca48c445ad86fce5
Fredy Garcia <fogarcia@labgencell.com>
2023-01-04 20:39:43 -0500

    Ajustes para copiar archivos por medio de sftp, pruebas ok

--- src/main/java/gencell/cronLaboratorio/controller/MBControllerLaboratorio.java
+++ src/main/java/gencell/cronLaboratorio/controller/MBControllerLaboratorio.java
@@ -20,6 +20,7 @@ import gencell.cronLaboratorio.selfdecode.ProfilePersonaSelfdecode;
 import gencell.cronLaboratorio.selfdecode.SelfdecodeServiceProcess;
 import java.io.File;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
@@ -38,7 +39,7 @@ public class MBControllerLaboratorio implements Serializable {
     public static final String PATH_ARCHIVOS_DEV = "public/files/dev/laboratorio_gp/ngs";
     //public static final String PATH_ARCHIVOS_SELFDECODE_DEV = "d:\\";
     //public static final String PATH_ARCHIVOS_SELFDECODE = "/var/www/html/files_selfdecode/";
-    public static final String PATH_ARCHIVOS_LOCAL = "C:\\Users\\devjava\\Desktop\\fas\\";
+    public static final String PATH_ARCHIVOS_LOCAL = "C:\\Users\\devjava\\Desktop\\laboratorio\\";
     public static final String HOST = "192.168.0.242";
     public static final String USUARIO = "linux";
     public static final String CONTRASENA = "linux";
@@ -47,33 +48,38 @@ public class MBControllerLaboratorio implements Serializable {
      * Creates a new instance of MBController
      */
     public MBControllerLaboratorio() {
-        if (sessionBeanBaseFachada == null) {
-            System.out.println("****************  fachada null Cron CARGA ARCHIVOS: ");
-            sessionBeanBaseFachada = this.lookupSessionBeanBaseFachadaLocal();
-            if (sessionBeanBaseFachada != null) {
-                System.out.println("****************  fachada obtenida Cron CARGA ARCHIVOS: ");
-            }
-        }
+//        if (sessionBeanBaseFachada == null) {
+//            System.out.println("****************  fachada null Cron CARGA ARCHIVOS: ");
+//            sessionBeanBaseFachada = this.lookupSessionBeanBaseFachadaLocal();
+//            if (sessionBeanBaseFachada != null) {
+//                System.out.println("****************  fachada obtenida Cron CARGA ARCHIVOS: ");
+//            }
+//        }
     }
 
     public void ejecutarTareaCargaArchivosLaboratorio() {
         try {
-            if (sessionBeanBaseFachada == null) {
-                System.out.println("**************** fachada null en ejecutar tarea CARGA ARCHIVOS");
-                return;
-            }
+//            if (sessionBeanBaseFachada == null) {
+//                System.out.println("**************** fachada null en ejecutar tarea CARGA ARCHIVOS");
+//                return;
+//            }
             Date fechaInicio = new Date();
-            //monitorCron = sessionBeanBaseFachada.BuscarPorID(Cron.class, 5);
             List<VWCronSelfdecodeCargaArchivos> listArchivosCargaSelfdecode;
-            listArchivosCargaSelfdecode = sessionBeanBaseFachada.BuscarTodos(VWCronSelfdecodeCargaArchivos.class);
-            if (listArchivosCargaSelfdecode != null && !listArchivosCargaSelfdecode.isEmpty()) {
-                System.out.println("**************** ENCUENTRA ARCHIVOS SELFDECODE PARA CARGAR: " + listArchivosCargaSelfdecode.size());
-                this.cargarArchivosSelfdecode(listArchivosCargaSelfdecode);
+            // CREAR UNA LISTA 
+            // listArchivosCargaSelfdecode = sessionBeanBaseFachada.BuscarTodos(VWCronSelfdecodeCargaArchivos.class);
+            
+            List<String> archivos = new ArrayList<>();
+            archivos.add("V350097149_L1_1.fq.gz");
+            archivos.add("V350097149_L1_2.fq.gz");
+            
+            if (archivos != null && !archivos.isEmpty()) {
+                System.out.println("**************** ENCUENTRA ARCHIVOS PARA CARGAR: " + archivos.size());
+                this.cargarArchivosFasq(archivos);
             }else{
                 System.out.println("No hay archivos para copiar");
             }
-            this.sendPost(); // Ejecutara el envio a Selfdecode 
-            this.eliminarArchivosSelfdecode();// Elimina los archivos que estan en estado Complete SELFDECODE
+            //this.sendPost(); // Ejecutara el envio a Selfdecode 
+            //this.eliminarArchivosSelfdecode();// Elimina los archivos que estan en estado Complete SELFDECODE
 //            if (monitorCron != null) {
 //                monitorCron.setFechaInicio(fechaInicio);
 //                monitorCron.setFechaFinal(new Date());
@@ -86,19 +92,21 @@ public class MBControllerLaboratorio implements Serializable {
         }
     }
 
-    private void cargarArchivosSelfdecode(List<VWCronSelfdecodeCargaArchivos> listArchivosCargaSelfdecode) {
-        for (int i = 0; i < listArchivosCargaSelfdecode.size(); i++) {
-            sessionBeanBaseFachada.actualizarEstadoPeticionBioLab(listArchivosCargaSelfdecode.get(i).getId(), "CARGANDO-SERVER", "10");
-        }
-        for (int i = 0; i < listArchivosCargaSelfdecode.size(); i++) {
-            VWCronSelfdecodeCargaArchivos archivoCargaSelf = listArchivosCargaSelfdecode.get(i);
+    private void cargarArchivosFasq(List<String> archivos) {
+//        for (int i = 0; i < listArchivosCargaSelfdecode.size(); i++) {
+//            sessionBeanBaseFachada.actualizarEstadoPeticionBioLab(listArchivosCargaSelfdecode.get(i).getId(), "CARGANDO-SERVER", "10");
+//        }
+        for (int i = 0; i < archivos.size(); i++) {
+            //VWCronSelfdecodeCargaArchivos archivoCargaSelf = listArchivosCargaSelfdecode.get(i);
             try {
-                Boolean flag = this.obtenerByteFile(archivoCargaSelf);
+                Boolean flag = this.obtenerByteFile(archivos.get(i));
                 if (flag != null && flag) {
-                    this.comprobarSize(archivoCargaSelf);
+                    this.comprobarSize(archivos.get(i));
+                }else{
+                    System.out.println(archivos.get(i)+ " NO SE PUEDE COPIAR");
                 }
             } catch (Exception ex) {
-                System.out.println("**************** ERROR AL CARGAR EL ARCHIVO DE CON ID: " + archivoCargaSelf.getId());
+                System.out.println("**************** ERROR AL CARGAR EL ARCHIVO : " + archivos.get(i));
                 ex.printStackTrace();
                 //sessionBeanBaseFachada.actualizarEstadoYDescPeticionBioLab(archivoCarga.getId(), "ERROR", ex.getMessage(), "12");
             }
@@ -106,7 +114,7 @@ public class MBControllerLaboratorio implements Serializable {
 
     }
 
-    private Boolean obtenerByteFile(VWCronSelfdecodeCargaArchivos archivoCar) throws Exception {
+    private Boolean obtenerByteFile(String archivoCar) throws Exception {
 
         JSch jsch = new JSch();
         ChannelSftp sftp = null;
@@ -121,13 +129,13 @@ public class MBControllerLaboratorio implements Serializable {
             sftp = (ChannelSftp) channel;
             sftp.connect();
             sftp.cd("/home/linux/Descargas/fas");
-            sftp.get(archivoCar.getNombreArchivo(), PATH_ARCHIVOS_LOCAL + archivoCar.getNombreArchivo());
+            sftp.get(archivoCar, PATH_ARCHIVOS_LOCAL + archivoCar);
             sftp.disconnect();
             return true;
         } catch (Exception e) {
             System.out.println("No se pudo realizar la conexión  para subir al server");
-            e.printStackTrace();
-            sessionBeanBaseFachada.actualizarEstadoYDescPeticionBioLab(archivoCar.getId(), "ERROR SUBIDA A SERVER", e.getMessage(), "15");
+            //e.printStackTrace();
+            //sessionBeanBaseFachada.actualizarEstadoYDescPeticionBioLab(archivoCar.getId(), "ERROR SUBIDA A SERVER", e.getMessage(), "15");
             return false;
         } finally {
             if (sftp != null) {
@@ -139,14 +147,14 @@ public class MBControllerLaboratorio implements Serializable {
         }
     }
 
-    private void comprobarSize(VWCronSelfdecodeCargaArchivos archivoCar) throws Exception {
+    private void comprobarSize(String archivoCar) throws Exception {
         JSch jsch = new JSch();
         ChannelSftp sftp = null;
         Session session = null;
         try {
-            File tmp = new File(PATH_ARCHIVOS_LOCAL + archivoCar.getNombreArchivo());
+            File tmp = new File(PATH_ARCHIVOS_LOCAL + archivoCar);
             if (!tmp.exists()) {
-                sessionBeanBaseFachada.actualizarEstadoYDescPeticionBioLab(archivoCar.getId(), "INICIAL", "EL ARCHIVO EN EL SERVIDOR NO EXISTE", "50");
+                //sessionBeanBaseFachada.actualizarEstadoYDescPeticionBioLab(archivoCar.getId(), "INICIAL", "EL ARCHIVO EN EL SERVIDOR NO EXISTE", "50");
                 return;
             }
 
@@ -162,13 +170,14 @@ public class MBControllerLaboratorio implements Serializable {
             for (int j = 0; j < flLst.size(); j++) {
                 ChannelSftp.LsEntry entry = flLst.get(j);
                 SftpATTRS attr = entry.getAttrs();
-                if (entry.getFilename().equals(archivoCar.getNombreArchivo())) {
+                if (entry.getFilename().equals(archivoCar)) {
                     if (tmp.length() == attr.getSize()) {
-                        sessionBeanBaseFachada.actualizarEstadoPeticionBioLab(archivoCar.getId(), "CARGADO-SERVER", "50");
+                        //sessionBeanBaseFachada.actualizarEstadoPeticionBioLab(archivoCar.getId(), "CARGADO-SERVER", "50");
+                        System.out.println("Archivo "+ archivoCar + " Copiado");
                         //this.sendPost(archivoCar);
                         break;
                     } else {
-                        sessionBeanBaseFachada.actualizarEstadoPeticionBioLab(archivoCar.getId(), "INICIAL", "10");
+                       // sessionBeanBaseFachada.actualizarEstadoPeticionBioLab(archivoCar.getId(), "INICIAL", "10");
                         tmp.delete();
                         break;
                     }
@@ -178,7 +187,7 @@ public class MBControllerLaboratorio implements Serializable {
         } catch (Exception e) {
             System.out.println("No se pudo realizar la conexión para comprobar archivo");
             e.printStackTrace();
-            sessionBeanBaseFachada.actualizarEstadoYDescPeticionBioLab(archivoCar.getId(), "ERROR COMPROBACION ARCHIVO", e.getMessage(), "15");
+            //sessionBeanBaseFachada.actualizarEstadoYDescPeticionBioLab(archivoCar.getId(), "ERROR COMPROBACION ARCHIVO", e.getMessage(), "15");
             return;
         } finally {
             if (sftp != null) {
@@ -190,70 +199,7 @@ public class MBControllerLaboratorio implements Serializable {
         }
     }
 
-    private void sendPost() {
-        //Variables
-        String forward_reads = "";
-        String reverse_reads = "";
-        Integer idPeticion = 0;
-        String profile;
-        Integer idPaciente;
-        ProfilePersonaSelfdecode profileSelfdecode;
-        SelfdecodeServiceProcess selfdecodeserviceprocess = new SelfdecodeServiceProcess();
-        List<VWCronSelfdecodeListos> listArchivosListosSelf;
-        List<VWCronSelfdecodeListos> archivosOkForwardAndReverse;
-
-        listArchivosListosSelf = sessionBeanBaseFachada.BuscarTodos(VWCronSelfdecodeListos.class);// Crea una lista con los objetos que estan listos 4 por consulta 2 envios a selfdecode por hilo
-        if (!listArchivosListosSelf.isEmpty() || listArchivosListosSelf != null) { // Verifica si se obtiene resultados listos para enviar 
-            for (int i = 0; i < listArchivosListosSelf.size(); i++) {
-               
-               // Limpiar Variables 
-               forward_reads = ""; 
-               reverse_reads = "";
-               profile = "";
-               
-                if (idPeticion != listArchivosListosSelf.get(i).getIdPeticion()) { // Valida si la peticion ya fue procesada
-
-                    idPeticion = listArchivosListosSelf.get(i).getIdPeticion(); // Obtiene la peticion 
-                    archivosOkForwardAndReverse = sessionBeanBaseFachada.obtenerArchivosListos(idPeticion); // consulta los que estan estado cargado-server, trae 2 registros o 1 
-
-                    // si aca despues de hacer la consulta no vienen los 2 no se ejecuta 
-                    if (archivosOkForwardAndReverse.size() == 2) {
-                        if (archivosOkForwardAndReverse.get(0).getIdentificador().equals("1")) {
-                            //listArchivosCargaSelfdecode = sessionBeanBaseFachada.obtenerArchivosSelfdecode(idPeticion); // deberia obtener solo 2 objetos
-                            forward_reads = PATH_ARCHIVOS_LOCAL + archivosOkForwardAndReverse.get(0).getNombreArchivo();
-                            reverse_reads = PATH_ARCHIVOS_LOCAL + archivosOkForwardAndReverse.get(1).getNombreArchivo();
-                            //System.out.println("Asignacion archivos 1");
-                        } else if (archivosOkForwardAndReverse.get(1).getIdentificador().equals("1")) {
-                            forward_reads = PATH_ARCHIVOS_LOCAL + archivosOkForwardAndReverse.get(1).getNombreArchivo();
-                            reverse_reads = PATH_ARCHIVOS_LOCAL + archivosOkForwardAndReverse.get(0).getNombreArchivo();
-                            //System.out.println("Asignacion Archivos 2");
-                        }
-
-                        profileSelfdecode = sessionBeanBaseFachada.consultarProfile(idPeticion); // Consulta el idPaciente y el Profile
-
-                        profile = profileSelfdecode.getIdProfileSelfdecode();
-                        idPaciente = profileSelfdecode.getIdPaciente();
-
-                        // Validación para que no este ningun parametro en nulo
-                        if ((profile != null) && (!forward_reads.equals("")) && (!reverse_reads.equals("")) && (idPaciente != null) && (idPeticion != null)) {
-                            sessionBeanBaseFachada.actualizarEstadoBiolabSelfdecode(idPeticion, "ENVIANDO-SELFDECODE", profile, "70");
-
-                            // Inicia el envio de datos a Selfdecode
-                            selfdecodeserviceprocess.iniciarEnvioArchivoSelfdecode(profile, forward_reads, reverse_reads, idPaciente, idPeticion);
-                        } else {
-                            System.out.println("Verifique que no tenga datos nulos");
-                        }
-                    } else {
-                        System.out.println("LOS ARCHIVOS NO ESTAN LISTOS O YA FUERON ENVIADOS");
-                    }
-                } else {
-                    System.out.println("La Peticion ya fue procesada");
-                }
-            }
-        } else {
-            System.out.println("Los Archivos no estan cargados");
-        }
-    }
+   
 
     public void eliminarArchivosSelfdecode() {
         // System.out.println("ELIMINAR ARCHIVOS");
